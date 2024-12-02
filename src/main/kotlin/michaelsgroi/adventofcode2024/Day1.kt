8650e0832d5a27824092deeffb65bc453cda2966
Mike Sgroi <msgroi@msgroi-ltmyc8b.internal.salesforce.com>
2024-12-01 19:25:46 -0800

    day1

--- src/main/kotlin/michaelsgroi/adventofcode2024/Day1.kt
+++ src/main/kotlin/michaelsgroi/adventofcode2024/Day1.kt
@@ -1,11 +1,35 @@
 package michaelsgroi.adventofcode2024
 
+import java.io.File
+import kotlin.math.abs
+
 class Day1 {
-    fun part1() {
-        TODO("Day1.part1() not implemented")
+    fun part1(filePath: String): Long {
+        val pairs =
+            File(filePath).readLines().map {
+                val values = it.split("   ")
+                Pair(values[0].toLong(), values[1].toLong())
+            }
+        val left = pairs.map { it.first }.sorted()
+        val right = pairs.map { it.second }.sorted()
+        return left
+            .zip(right)
+            .sumOf { abs(it.second - it.first) }
     }
 
-    fun part2() {
-        TODO("Day1.part2() not implemented")
+    fun part2(filePath: String): Long {
+        val pairs =
+            File(filePath).readLines().map {
+                val values = it.split("   ")
+                Pair(values[0].toLong(), values[1].toLong())
+            }
+        val right = pairs.map { it.second }
+        val rightOccurrenceCount = right.groupingBy { it }.eachCount()
+        val left = pairs.map { it.first }
+        val similarityScores =
+            left.map { leftValue ->
+                leftValue * (rightOccurrenceCount[leftValue] ?: 0)
+            }
+        return similarityScores.sum()
     }
 }
--- src/test/kotlin/michaelsgroi/adventofcode2024/Day1Tester.kt
+++ src/test/kotlin/michaelsgroi/adventofcode2024/Day1Tester.kt
@@ -1,15 +1,21 @@
 package michaelsgroi.adventofcode2024
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Day1Tester {
+    private val test = "src/main/resources/day1_test"
+    private val live = "src/main/resources/day1_live"
+
     @Test
     fun part1() {
-        Day1().part1()
+        assertEquals(11, Day1().part1(test))
+        assertEquals(2192892, Day1().part1(live))
     }
 
     @Test
     fun part2() {
-        Day1().part2()
+        assertEquals(31, Day1().part2(test))
+        assertEquals(22962826, Day1().part2(live))
     }
 }
