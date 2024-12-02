a33c196b8c3c187a01ae22fd41fbc808fc896c83
Mike Sgroi <msgroi@msgroi-ltmyc8b.internal.salesforce.com>
2024-12-01 19:32:28 -0800

    tidying up

--- src/main/kotlin/michaelsgroi/adventofcode2024/Day1.kt
+++ src/main/kotlin/michaelsgroi/adventofcode2024/Day1.kt
@@ -5,31 +5,29 @@ import kotlin.math.abs
 
 class Day1 {
     fun part1(filePath: String): Long {
-        val pairs =
-            File(filePath).readLines().map {
-                val values = it.split("   ")
-                Pair(values[0].toLong(), values[1].toLong())
-            }
-        val left = pairs.map { it.first }.sorted()
-        val right = pairs.map { it.second }.sorted()
+        val (left, right) = filePath.toLeftAndRight()
         return left
-            .zip(right)
+            .sorted()
+            .zip(right.sorted())
             .sumOf { abs(it.second - it.first) }
     }
 
     fun part2(filePath: String): Long {
-        val pairs =
-            File(filePath).readLines().map {
-                val values = it.split("   ")
-                Pair(values[0].toLong(), values[1].toLong())
-            }
-        val right = pairs.map { it.second }
+        val (left, right) = filePath.toLeftAndRight()
         val rightOccurrenceCount = right.groupingBy { it }.eachCount()
-        val left = pairs.map { it.first }
         val similarityScores =
             left.map { leftValue ->
                 leftValue * (rightOccurrenceCount[leftValue] ?: 0)
             }
         return similarityScores.sum()
+    }
+
+    private fun String.toLeftAndRight(): Pair<List<Long>, List<Long>> {
+        val pairs =
+            File(this).readLines().map {
+                val values = it.split("   ")
+                Pair(values[0].toLong(), values[1].toLong())
+            }
+        return Pair(pairs.map { it.first }, pairs.map { it.second })
     }
 }
